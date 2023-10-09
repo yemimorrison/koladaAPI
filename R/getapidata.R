d2247e2e55406fe3d139274435d327e188e10b00
Yemi Morrison <morrisonolayemi@gmail.com>
2023-10-09 16:26:30 +0200

    Cleaned data extracted via API. Built shiny web app and created repository.

--- R/getapidata.R
+++ R/getapidata.R
@@ -11,9 +11,18 @@ get_api_data <- function(endpoint, entity, params = list()) {
   
   
   url <- paste0(endpoint, "/", entity, "?", paste0(names(params), "=", params, collapse = "&"))
-  response <- httr::GET(url)
-  httr::content(response, "parsed")
+  
+  return(url)
+  
+  #response <- httr::GET(url)
+  #httr::content(response, "parsed")
+  
 }
 
-api_data <- get_api_data("http://api.kolada.se/v2", entity = "municipality", params = list(title = "lund"))
-api_data
+api_data <- get_api_data("http://api.kolada.se/v2", entity = "municipality", params = list())
+api_data_df <- fromJSON(api_data)
+api_data_df <- as.data.frame(api_data_df)
+
+kpi_data <- get_api_data("http://api.kolada.se/v2", entity = "kpi", params = list())
+kpi_data_df <- fromJSON(kpi_data)
+kpi_data_df <- as.data.frame(kpi_data_df)
--- R/getquerydata.R
+++ R/getquerydata.R
@@ -4,8 +4,9 @@
 #' @param endpoint The API endpoint URL
 #' @param data_api The type of data description
 #' @param entities   A list of query parameters
+#' 
 #' @return         Parsed API response
-#'
+#' 
 #' @export
 
 get_query_data <- function(endpoint, data_api, entities) {
@@ -25,10 +26,10 @@ get_query_data <- function(endpoint, data_api, entities) {
     # Construct the full API endpoint URL
     url <- paste0(endpoint, "/", data_api, "/", paste(entities, collapse = "/"))
     
-    #return(url)
+    return(url)
     
-    response <- httr::GET(url)
-    httr::content(response, "parsed")
+    #response <- httr::GET(url)
+    #httr::content(response, "parsed")
   } else {
     stop("Invalid number of entities. Provide 2 or 3 entities.")
   }
@@ -36,11 +37,35 @@ get_query_data <- function(endpoint, data_api, entities) {
   
 }
 
+
 # Example usage for data_api = "data" with two entities
-query_data <- get_query_data("http://api.kolada.se/v2", data_api = "data", entities = c("municipality", "1860", "year", "2009"))
-query_data
+query_data <- get_query_data("http://api.kolada.se/v2", data_api = "data", entities = c("kpi", "N03932","municipality", "0580,1480,1280,0180,0380,1980"))
 
 # Example usage for data_api = "oudata" with three entities
-query_data <- get_query_data("http://api.kolada.se/v2", data_api = "oudata", entities = c("kpi", "N15033,N15030", "ou", "V15E144001301,V15E144001101", "year", "2009,2008,2007"))
-query_data
+#query_data <- get_query_data("http://api.kolada.se/v2", data_api = "oudata", entities = c("kpi", "N15033", "ou", "V15E144001301,V15E144001101", "year", "2009,2007"))
+#query_data
+
+query_data_frame <- fromJSON(query_data)
+query_data_frame <- as.data.frame(query_data_frame)
+
+for (i in 1:nrow(query_data_frame)){
+  sub_df <- query_data_frame[[5]][[i]]
+  query_data_frame[i, 5] <- sub_df[3,4]
+  }
+  
+description <- c("Count", "KPI", "MunicipalityID", "Year", "UnemploymentRate")
+colnames(query_data_frame)<- description
+
+query_data_frame <- query_data_frame %>% 
+  mutate(Municipality = case_when(
+    MunicipalityID == "0180" ~ "Stockholm",
+    MunicipalityID == "1480" ~ "Goteborg",
+    MunicipalityID == "0580" ~ "Linkoping",
+    MunicipalityID == "0380" ~ "Uppsala",
+    MunicipalityID == "1280" ~ "Malmo",
+    MunicipalityID == "1980" ~ "Vasteras",
+    TRUE ~ NA_character_
+  ))
+
+
 
